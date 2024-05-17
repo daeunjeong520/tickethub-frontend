ef5b258155f464e532bcb643b75a281125cb57e6
Daeun Jeong <jungde1345@gmail.com>
2024-05-17 10:38:09 +0900

    update

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -4,7 +4,7 @@
   <section class="py-5 text-center container">
     <div class="row py-lg-5">
       <div class="col-lg-6 col-md-8 mx-auto">
-        <h1 class="fw-light">이번주 HOT</h1>
+        <h1 class="fw-light">이번주 HOTT</h1>
         <h2 class="lead text-body-secondary"></h2>
       </div>
     </div>
